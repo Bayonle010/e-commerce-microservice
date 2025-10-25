1263b2aa904630996bcd860c1dbe9c4ce84274ea
Bayonle <afeezonadiran@gmail.com>
2025-10-25 11:34:35 +0100

    added product micro service

---
+++ services/product/src/test/java/com/bayo/ecommerce/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bayo.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
