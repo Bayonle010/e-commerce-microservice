ac67f8520c275fc691defc8a02bb55aec65a27b7
Bayonle <afeezonadiran@gmail.com>
2025-10-25 04:10:29 +0100

    create customer

---
+++ services/customer/src/main/java/com/bayo/ecommerce/CustomerService.java
@@ -0,0 +1,16 @@
+package com.bayo.ecommerce;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerService {
+    private final CustomerRepository repository;
+    private final CustomerMapper mapper;
+
+    public String createCustomer(CustomerRequest request){
+        var customer = this.repository.save(mapper.toCustomer(request));
+        return customer.getId();
+    }
+}
