8d4faf42a01ed3580d0b825fdae8436da10cad8d
Bayonle <afeezonadiran@gmail.com>
2026-01-22 01:39:40 +0100

    imports order microservice

---
+++ services/order/src/test/java/com/bayo/ecommerce/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bayo.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
