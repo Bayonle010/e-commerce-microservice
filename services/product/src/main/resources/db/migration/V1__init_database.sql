d4b617ccce51b6cd43ebd44ed18dc2c68d7886d3
Bayonle <afeezonadiran@gmail.com>
2026-01-22 01:04:43 +0100

    seeds data to product

--- services/product/src/main/resources/db/migration/V1__init_database.sql
+++ services/product/src/main/resources/db/migration/V1__init_database.sql
@@ -9,7 +9,7 @@ create table if not exists product
 (
    id   integer  not null  primary key,
    available_quantity double precision not null,
-   description        varcharg(255),
+   description        varchar(255),
    name               varchar(255),
    price              numeric(38, 2),
    category_id        integer
